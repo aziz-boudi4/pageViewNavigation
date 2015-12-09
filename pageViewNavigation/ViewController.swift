306aa196170aee3e6c3a472feb36296269f18803
Aziz Boudi <azicode4@gmail.com>
2015-12-08 23:02:44 -0800

    - added a scroll view

--- pageViewNavigation/ViewController.swift
+++ pageViewNavigation/ViewController.swift
@@ -10,8 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var scrollView: UIScrollView!
+
   override func viewDidLoad() {
     super.viewDidLoad()
+
+    scrollView.pagingEnabled = true
+
+
     // Do any additional setup after loading the view, typically from a nib.
   }
 
